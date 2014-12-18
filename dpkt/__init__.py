2060f6d27efe660aae61ea503236e97e0ec3418b
Brian Wylie <bwylie@vectranetworks.com>
2014-12-18 11:54:29 -0700

    putting version to 1.8.2

--- dpkt/__init__.py
+++ dpkt/__init__.py
@@ -5,5 +5,5 @@ __author__ = 'Dug Song <dugsong@monkey.org>'
 __copyright__ = 'Copyright (c) 2004 Dug Song'
 __license__ = 'BSD'
 __url__ = 'http://dpkt.googlecode.com/'
-__version__ = '1.8.1'
+__version__ = '1.8.2'
 
