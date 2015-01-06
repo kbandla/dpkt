2d47765f3ea6580fa783d658c80d48e8b89f2cb8
Brian Wylie <bwylie@vectranetworks.com>
2015-01-06 14:59:04 -0800

    adding Debian Packaging to setup.py

--- dpkt/__init__.py
+++ dpkt/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Dug Song <dugsong@monkey.org>'
 __copyright__ = 'Copyright (c) 2004 Dug Song'
 __license__ = 'BSD'
 __url__ = 'http://dpkt.googlecode.com/'
-__version__ = '1.8.5'
+__version__ = '1.8.6'
 
 from dpkt import *
 
