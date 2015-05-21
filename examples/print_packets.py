027fcc84862672b3c5d22b2a73e387966b49a361
Brian Wylie <bwylie@vectranetworks.com>
2015-05-20 20:09:45 -0600

    fixing landscape minutia

--- examples/print_packets.py
+++ examples/print_packets.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python
-"""Use DPKT to read in a pcap file and print out the contents of the packets
-   This example is focused on the fields in the Ethernet Frame and IP packet"""
+"""
+    Use DPKT to read in a pcap file and print out the contents of the packets
+    This example is focused on the fields in the Ethernet Frame and IP packet
+"""
 import dpkt
 import datetime
 import socket
 
 def mac_addr(mac_string):
     """Print out MAC address given a string
-        Args:
-            mac_string: the string representation of a MAC address
-        Returns:
-            printable MAC address
+
+       Args:
+           mac_string: the string representation of a MAC address
+       Returns:
+           printable MAC address
     """    
     return ':'.join('%02x' % ord(b) for b in mac_string)
 
 def ip_to_str(address):
     """Print out an IP address given a string
-        Args:
-            address: the string representation of a MAC address
-        Returns:
-            printable IP address
+
+       Args:
+           address: the string representation of a MAC address
+       Returns:
+           printable IP address
     """    
     return socket.inet_ntop(socket.AF_INET, address)
 
 def print_packets(pcap):
     """Print out information about each packet in a pcap
-        Args:
-            pcap: dpkt pcap reader object (dpkt.pcap.Reader)
+
+       Args:
+           pcap: dpkt pcap reader object (dpkt.pcap.Reader)
     """
 
     # For each packet in the pcap process the contents
