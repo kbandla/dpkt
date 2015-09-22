abc992bc8fddc7b0ad75040416db1cf1252d6975
Andrew McConachie <smutt@depht.com>
2015-09-22 15:53:34 -0700

    Fixed some spacing issues in dot1q.py

--- dpkt/dot1q.py
+++ dpkt/dot1q.py
@@ -4,20 +4,20 @@ import struct
 import dpkt
 
 # Ethernet payload types - http://standards.ieee.org/regauth/ethertype
-ETH_TYPE_PUP	= 0x0200		# PUP protocol
-ETH_TYPE_IP	= 0x0800		# IP protocol
-ETH_TYPE_ARP	= 0x0806		# address resolution protocol
-ETH_TYPE_CDP	= 0x2000		# Cisco Discovery Protocol
-ETH_TYPE_DTP	= 0x2004		# Cisco Dynamic Trunking Protocol
-ETH_TYPE_REVARP	= 0x8035		# reverse addr resolution protocol
-ETH_TYPE_DOT1Q	= 0x8100		# IEEE 802.1Q VLAN tagging
-ETH_TYPE_IPX	= 0x8137		# Internetwork Packet Exchange
-ETH_TYPE_IP6	= 0x86DD		# IPv6 protocol
-ETH_TYPE_PPP	= 0x880B		# PPP
-ETH_TYPE_MPLS	= 0x8847		# MPLS
-ETH_TYPE_MPLS_MCAST	= 0x8848	# MPLS Multicast
-ETH_TYPE_PPPoE_DISC	= 0x8863	# PPP Over Ethernet Discovery Stage
-ETH_TYPE_PPPoE		= 0x8864	# PPP Over Ethernet Session Stage
+ETH_TYPE_PUP = 0x0200  # PUP protocol
+ETH_TYPE_IP = 0x0800  # IP protocol
+ETH_TYPE_ARP = 0x0806  # address resolution protocol
+ETH_TYPE_CDP = 0x2000  # Cisco Discovery Protocol
+ETH_TYPE_DTP = 0x2004  # Cisco Dynamic Trunking Protocol
+ETH_TYPE_REVARP = 0x8035  # reverse addr resolution protocol
+ETH_TYPE_DOT1Q = 0x8100  # IEEE 802.1Q VLAN tagging
+ETH_TYPE_IPX = 0x8137  # Internetwork Packet Exchange
+ETH_TYPE_IP6 = 0x86DD  # IPv6 protocol
+ETH_TYPE_PPP = 0x880B  # PPP
+ETH_TYPE_MPLS = 0x8847  # MPLS
+ETH_TYPE_MPLS_MCAST = 0x8848  # MPLS Multicast
+ETH_TYPE_PPPoE_DISC = 0x8863  # PPP Over Ethernet Discovery Stage
+ETH_TYPE_PPPoE = 0x8864  # PPP Over Ethernet Session Stage
 
 class DOT1Q(dpkt.Packet):
     __hdr__ = (
@@ -73,7 +73,6 @@ class DOT1Q(dpkt.Packet):
     def unpack(self, buf):
         dpkt.Packet.unpack(self, buf)
         self._unpack_data(self.data)
-
 
 # XXX - auto-load Ethernet dispatch table from ETH_TYPE_* definitions
 def __load_types():
