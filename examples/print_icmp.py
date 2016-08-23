e157728653b3762bed481bf34028c7e78db8a775
Brian Wylie <briford.wylie@gmail.com>
2016-08-22 19:11:29 -0600

    adding an ICMP example script

--- examples/print_icmp.py
+++ examples/print_icmp.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
-Use DPKT to read in a pcap file and print out the contents of the packets
-This example is focused on the fields in the Ethernet Frame and IP packet
+This example expands on the print_packets example. It checks for IMCP packets and displays the ICMP contents.
 """
 import dpkt
 import datetime
@@ -33,7 +32,7 @@ def inet_to_str(inet):
     except ValueError:
         return socket.inet_ntop(socket.AF_INET6, inet)
 
-def print_packets(pcap):
+def print_icmp(pcap):
     """Print out information about each packet in a pcap
 
        Args:
@@ -42,12 +41,8 @@ def print_packets(pcap):
     # For each packet in the pcap process the contents
     for timestamp, buf in pcap:
 
-        # Print out the timestamp in UTC
-        print 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp))
-
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
-        print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
 
         # Make sure the Ethernet frame contains an IP packet
         # EtherType (IP, ARP, PPPoE, IP6... see http://en.wikipedia.org/wiki/EtherType)
@@ -55,25 +50,31 @@ def print_packets(pcap):
             print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
             continue
 
-        # Now unpack the data within the Ethernet frame (the IP packet)
-        # Pulling out src, dst, length, fragment info, TTL, and Protocol
+        # Now grab the data within the Ethernet frame (the IP packet)
         ip = eth.data
 
-        # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
-        do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
-        more_fragments = bool(ip.off & dpkt.ip.IP_MF)
-        fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+        # Now check if this is an ICMP packet
+        if hasattr(ip, 'data') and ip.data.__class__.__name__ == 'ICMP':
+            icmp = ip.data
 
-        # Print out the info
-        print 'IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
-              (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset)
+            # Pull out fragment information (flags and offset all packed into off field, so use bitmasks)
+            do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
+            more_fragments = bool(ip.off & dpkt.ip.IP_MF)
+            fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
+
+            # Print out the info
+            print 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp))
+            print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
+            print 'IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)' % \
+                  (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset)
+            print 'ICMP: type:%d code:%d checksum:%d data: %s\n' % (icmp.type, icmp.code, icmp.sum, repr(icmp.data))
 
 
 def test():
     """Open up a test pcap file and print out the packets"""
-    with open('data/http.pcap', 'rb') as f:
+    with open('data/dns_icmp.pcap', 'rb') as f:
         pcap = dpkt.pcap.Reader(f)
-        print_packets(pcap)
+        print_icmp(pcap)
 
 
 if __name__ == '__main__':
