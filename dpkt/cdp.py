d19f01ca2fd921f99bce74152da06735a979d055
Mori Benech <moribirom@gmail.com>
2017-03-22 08:19:16 +0200

    modified:   dpkt/cdp.py Improve cdp : more fields handled, well formed packets, and tests added

--- dpkt/cdp.py
+++ dpkt/cdp.py
@@ -43,7 +43,36 @@ class CDP(dpkt.Packet):
         ('sum', 'H', 0)
     )
 
-    class Address(dpkt.Packet):
+    #keep here the TLV classes whose header is different from the generic TLV header (example : TLV_Addresses)
+    tlv_types = {CDP_ADDRESS: 'TLV_Addresses'}
+
+    class TLV(dpkt.Packet):
+        '''When constructing the packet, len is not mandatory : if not provided, then self.data must be this exact TLV payload'''
+
+        __hdr__ = (
+            ('type', 'H', 0),
+            ('len', 'H', 0)
+        )
+
+        def data_len(self):
+            if self.len:
+                return self.len - self.__hdr_len__
+            return len(self.data)
+
+        def unpack(self, buf):
+            dpkt.Packet.unpack(self, buf)
+            self.data = self.data[:self.data_len()]
+
+        def __len__(self):
+            return self.__hdr_len__ + len(self.data)
+
+        def __bytes__(self):
+            if hasattr(self,'len') and not self.len:
+                self.len = len(self)
+            return self.pack_hdr() + bytes(self.data)
+
+
+    class Address(TLV):
         # XXX - only handle NLPID/IP for now
         __hdr__ = (
             ('ptype', 'B', 1),  # protocol type (NLPID)
@@ -51,61 +80,61 @@ class CDP(dpkt.Packet):
             ('p', 'B', 0xcc),  # IP
             ('alen', 'H', 4)  # address length
         )
+        def data_len(self):
+            return self.alen
 
-        def unpack(self, buf):
-            dpkt.Packet.unpack(self, buf)
-            self.data = self.data[:self.alen]
 
-    class TLV(dpkt.Packet):
+    class TLV_Addresses(TLV):
         __hdr__ = (
-            ('type', 'H', 0),
-            ('len', 'H', 4)
+            ('type', 'H', CDP_ADDRESS),
+            ('len', 'H', 0),    #17),
+            ('Addresses', 'L', 1),
         )
 
-        def unpack(self, buf):
-            dpkt.Packet.unpack(self, buf)
-            self.data = self.data[:self.len - 4]
-            if self.type == CDP_ADDRESS:
-                n = struct.unpack('>I', self.data[:4])[0]
-                buf = self.data[4:]
-                l = []
-                for i in range(n):
-                    a = CDP.Address(buf)
-                    l.append(a)
-                    buf = buf[len(a):]
-                self.data = l
-
-        def __len__(self):
-            if self.type == CDP_ADDRESS:
-                n = 4 + sum(map(len, self.data))
-            else:
-                n = len(self.data)
-            return self.__hdr_len__ + n
-
-        def __bytes__(self):
-            self.len = len(self)
-            if self.type == CDP_ADDRESS:
-                s = struct.pack('>I', len(self.data)) + \
-                    b''.join(map(bytes, self.data))
-            else:
-                s = self.data
-            return self.pack_hdr() + s
 
     def unpack(self, buf):
         dpkt.Packet.unpack(self, buf)
         buf = self.data
         l = []
         while buf:
-            tlv = self.TLV(buf)
-            l.append(tlv)
+            #find the right TLV according to Type value
+            tlv_find_type = self.TLV(buf).type
+            #if this TLV is not in tlv_types, use the default TLV class
+            tlv = getattr(self, self.tlv_types.get(tlv_find_type, 'TLV'))(buf)
+            l.append(bytes(tlv))
             buf = buf[len(tlv):]
-        self.data = l
+        self.data = b''.join(l)
 
     def __len__(self):
         return self.__hdr_len__ + sum(map(len, self.data))
 
     def __bytes__(self):
-        data = b''.join(map(bytes, self.data))
+        data = bytes(self.data)
         if not self.sum:
             self.sum = dpkt.in_cksum(self.pack_hdr() + data)
         return self.pack_hdr() + data
+
+
+def test_cdp():
+    import socket
+
+    ss = (b'\x02\xb4\xdf\x93\x00\x01\x00\x09\x63\x69\x73\x63\x6f\x00\x02\x00\x11\x00\x00\x00\x01\x01\x01\xcc\x00\x04\xc0\xa8\x01\x67')
+    rr1 = CDP(ss)
+    assert bytes(rr1) == ss    
+
+    # construction
+    ss = (b'\x02\xb4\xdf\x93\x00\x01\x00\x09\x63\x69\x73\x63\x6f\x00\x02\x00\x11\x00\x00\x00\x01\x01\x01\xcc\x00\x04\xc0\xa8\x01\x67')
+    p1 = CDP.TLV_Addresses(data=CDP.Address(data=socket.inet_aton('192.168.1.103')))
+    p2 = CDP.TLV(type=CDP_DEVID, data=b'cisco')
+    data = p2.pack() + p1.pack()
+    rr2 = CDP(data=data)
+    assert bytes(rr2) == ss    
+
+
+
+if __name__ == '__main__':
+    test_cdp()
+    print('Tests Successful...')
+
+
+
