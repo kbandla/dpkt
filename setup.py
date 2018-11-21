9cb5deb0459ee3d43d9b26405c328d8c91273c3d
Brian Wylie <briford.wylie@gmail.com>
2018-11-21 14:01:10 -0700

    adding 3.6 to support versions

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(name=package_name,
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
       ]
