263dd3c7440313a248597580779510e05d1b8fba
Yegor Yefremov <yegorslists@googlemail.com>
2018-12-10 09:04:40 +0100

    Fix project's URL
    Google Code link is dead so point to GitHub repository.

--- dpkt/__init__.py
+++ dpkt/__init__.py
@@ -6,7 +6,7 @@ import sys
 __author__ = 'Dug Song'
 __author_email__ = 'dugsong@monkey.org'
 __license__ = 'BSD'
-__url__ = 'http://dpkt.googlecode.com/'
+__url__ = 'https://github.com/kbandla/dpkt'
 __version__ = '1.9.2'
 
 
