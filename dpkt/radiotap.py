875fc6be2f9940a40dd8ef0b895b2999a4b4bb1b
Joel 'Aaron' Cohen <joel.cohen@nrl.navy.mil>
2020-07-10 15:35:43 -0400

    Improve radiotap situation
    Correct endianness issues
    Support multi-byte present flags
    Handle misaligned flags fields

--- dpkt/radiotap.py
+++ dpkt/radiotap.py
@@ -12,41 +12,23 @@ from . import ieee80211
 # Fields Ref: http://www.radiotap.org/defined-fields/all
 
 # Present flags
-_TSFT_MASK = 0x1000000
-_FLAGS_MASK = 0x2000000
-_RATE_MASK = 0x4000000
-_CHANNEL_MASK = 0x8000000
-_FHSS_MASK = 0x10000000
-_ANT_SIG_MASK = 0x20000000
-_ANT_NOISE_MASK = 0x40000000
-_LOCK_QUAL_MASK = 0x80000000
-_TX_ATTN_MASK = 0x10000
-_DB_TX_ATTN_MASK = 0x20000
-_DBM_TX_POWER_MASK = 0x40000
-_ANTENNA_MASK = 0x80000
-_DB_ANT_SIG_MASK = 0x100000
-_DB_ANT_NOISE_MASK = 0x200000
-_RX_FLAGS_MASK = 0x400000
-_CHANNELPLUS_MASK = 0x200
-_EXT_MASK = 0x1
-
-_TSFT_SHIFT = 24
-_FLAGS_SHIFT = 25
-_RATE_SHIFT = 26
-_CHANNEL_SHIFT = 27
-_FHSS_SHIFT = 28
-_ANT_SIG_SHIFT = 29
-_ANT_NOISE_SHIFT = 30
-_LOCK_QUAL_SHIFT = 31
-_TX_ATTN_SHIFT = 16
-_DB_TX_ATTN_SHIFT = 17
-_DBM_TX_POWER_SHIFT = 18
-_ANTENNA_SHIFT = 19
-_DB_ANT_SIG_SHIFT = 20
-_DB_ANT_NOISE_SHIFT = 21
-_RX_FLAGS_SHIFT = 22
-_CHANNELPLUS_SHIFT = 10
-_EXT_SHIFT = 0
+_TSFT_SHIFT = 0
+_FLAGS_SHIFT = 1
+_RATE_SHIFT = 2
+_CHANNEL_SHIFT = 3
+_FHSS_SHIFT = 4
+_ANT_SIG_SHIFT = 5
+_ANT_NOISE_SHIFT = 6
+_LOCK_QUAL_SHIFT = 7
+_TX_ATTN_SHIFT = 8
+_DB_TX_ATTN_SHIFT = 9
+_DBM_TX_POWER_SHIFT = 10
+_ANTENNA_SHIFT = 11
+_DB_ANT_SIG_SHIFT = 12
+_DB_ANT_NOISE_SHIFT = 13
+_RX_FLAGS_SHIFT = 14
+_CHANNELPLUS_SHIFT = 18
+_EXT_SHIFT = 31
 
 # Flags elements
 _FLAGS_SIZE = 2
@@ -75,7 +57,7 @@ _HALF_RATE_SHIFT = 14
 _QUARTER_RATE_SHIFT = 15
 
 # Flags offsets and masks
-_FCS_SHIFT = 4
+_FCS_SHIFT = 1
 _FCS_MASK = 0x10
 
 
@@ -93,12 +75,18 @@ class Radiotap(dpkt.Packet):
         ('version', 'B', 0),
         ('pad', 'B', 0),
         ('length', 'H', 0),
-        ('present_flags', 'I', 0)
     )
+
+    __byte_order__ = '<'
+
+    def is_present(self, bit):
+        index = bit // 8
+        mask = 1 << (bit % 8)
+        return self.present_flags[index] & mask
 
     @property
     def tsft_present(self):
-        return (self.present_flags & _TSFT_MASK) >> _TSFT_SHIFT
+        return self.is_present(_TSFT_SHIFT)
 
     @tsft_present.setter
     def tsft_present(self, val):
@@ -106,7 +94,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def flags_present(self):
-        return (self.present_flags & _FLAGS_MASK) >> _FLAGS_SHIFT
+        return self.is_present(_FLAGS_SHIFT)
 
     @flags_present.setter
     def flags_present(self, val):
@@ -114,7 +102,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def rate_present(self):
-        return (self.present_flags & _RATE_MASK) >> _RATE_SHIFT
+        return self.is_present(_RATE_SHIFT)
 
     @rate_present.setter
     def rate_present(self, val):
@@ -122,7 +110,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def channel_present(self):
-        return (self.present_flags & _CHANNEL_MASK) >> _CHANNEL_SHIFT
+        return self.is_present(_CHANNEL_SHIFT)
 
     @channel_present.setter
     def channel_present(self, val):
@@ -130,7 +118,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def fhss_present(self):
-        return (self.present_flags & _FHSS_MASK) >> _FHSS_SHIFT
+        return self.is_present(_FHSS_SHIFT)
 
     @fhss_present.setter
     def fhss_present(self, val):
@@ -138,7 +126,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def ant_sig_present(self):
-        return (self.present_flags & _ANT_SIG_MASK) >> _ANT_SIG_SHIFT
+        return self.is_present(_ANT_SIG_SHIFT)
 
     @ant_sig_present.setter
     def ant_sig_present(self, val):
@@ -146,7 +134,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def ant_noise_present(self):
-        return (self.present_flags & _ANT_NOISE_MASK) >> _ANT_NOISE_SHIFT
+        return self.is_present(_ANT_NOISE_SHIFT)
 
     @ant_noise_present.setter
     def ant_noise_present(self, val):
@@ -154,7 +142,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def lock_qual_present(self):
-        return (self.present_flags & _LOCK_QUAL_MASK) >> _LOCK_QUAL_SHIFT
+        return self.is_present(_LOCK_QUAL_SHIFT)
 
     @lock_qual_present.setter
     def lock_qual_present(self, val):
@@ -162,7 +150,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def tx_attn_present(self):
-        return (self.present_flags & _TX_ATTN_MASK) >> _TX_ATTN_SHIFT
+        return self.is_present(_TX_ATTN_SHIFT)
 
     @tx_attn_present.setter
     def tx_attn_present(self, val):
@@ -170,7 +158,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def db_tx_attn_present(self):
-        return (self.present_flags & _DB_TX_ATTN_MASK) >> _DB_TX_ATTN_SHIFT
+        return self.is_present(_DB_TX_ATTN_SHIFT)
 
     @db_tx_attn_present.setter
     def db_tx_attn_present(self, val):
@@ -178,7 +166,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def dbm_tx_power_present(self):
-        return (self.present_flags & _DBM_TX_POWER_MASK) >> _DBM_TX_POWER_SHIFT
+        return self.is_present(_DBM_TX_POWER_SHIFT)
 
     @dbm_tx_power_present.setter
     def dbm_tx_power_present(self, val):
@@ -186,7 +174,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def ant_present(self):
-        return (self.present_flags & _ANTENNA_MASK) >> _ANTENNA_SHIFT
+        return self.is_present(_ANTENNA_SHIFT)
 
     @ant_present.setter
     def ant_present(self, val):
@@ -194,7 +182,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def db_ant_sig_present(self):
-        return (self.present_flags & _DB_ANT_SIG_MASK) >> _DB_ANT_SIG_SHIFT
+        return self.is_present(_DB_ANT_SIG_SHIFT)
 
     @db_ant_sig_present.setter
     def db_ant_sig_present(self, val):
@@ -202,7 +190,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def db_ant_noise_present(self):
-        return (self.present_flags & _DB_ANT_NOISE_MASK) >> _DB_ANT_NOISE_SHIFT
+        return self.is_present(_DB_ANT_NOISE_SHIFT)
 
     @db_ant_noise_present.setter
     def db_ant_noise_present(self, val):
@@ -210,7 +198,7 @@ class Radiotap(dpkt.Packet):
 
     @property
     def rx_flags_present(self):
-        return (self.present_flags & _RX_FLAGS_MASK) >> _RX_FLAGS_SHIFT
+        return self.is_present(_RX_FLAGS_SHIFT)
 
     @rx_flags_present.setter
     def rx_flags_present(self, val):
@@ -218,26 +206,26 @@ class Radiotap(dpkt.Packet):
 
     @property
     def chanplus_present(self):
-        return (self.present_flags & _CHANNELPLUS_MASK) >> _CHANNELPLUS_SHIFT
+        return self.is_present(_CHANNELPLUS_SHIFT)
 
     @chanplus_present.setter
     def chanplus_present(self, val):
         self.present_flags |= val << _CHANNELPLUS_SHIFT
 
-    @property
-    def ext_present(self):
-        return (self.present_flags & _EXT_MASK) >> _EXT_SHIFT
-
-    @ext_present.setter
-    def ext_present(self, val):
-        self.present_flags |= val << _EXT_SHIFT
-
     def unpack(self, buf):
         dpkt.Packet.unpack(self, buf)
-        self.data = buf[socket.ntohs(self.length):]
+        self.data = buf[self.length:]
 
         self.fields = []
         buf = buf[self.__hdr_len__:]
+
+        self.present_flags = bytearray(buf[:4])
+        buf = buf[4:]
+        ext_bit = _EXT_SHIFT
+        while self.is_present(ext_bit):
+            self.present_flags = self.present_flags + bytearray(buf[:4])
+            buf = buf[4:]
+            ext_bit += 32
 
         # decode each field into self.<name> (eg. self.tsft) as well as append it self.fields list
         field_decoder = [
@@ -257,8 +245,15 @@ class Radiotap(dpkt.Packet):
             ('db_ant_noise', self.db_ant_noise_present, self.DbAntennaNoise),
             ('rx_flags', self.rx_flags_present, self.RxFlags)
         ]
+
+        offset = self.__hdr_len__ + len(self.present_flags)
+
         for name, present_bit, parser in field_decoder:
             if present_bit:
+                if parser.__alignment__ > 1:
+                    padding = offset % parser.__alignment__
+                    buf = buf[padding:]
+                    offset += padding
                 field = parser(buf)
                 field.data = b''
                 setattr(self, name, field)
@@ -271,34 +266,39 @@ class Radiotap(dpkt.Packet):
             else:
                 self.data = ieee80211.IEEE80211(self.data)
 
-    class Antenna(dpkt.Packet):
+    class RadiotapField(dpkt.Packet):
+        __alignment__ = 1
+        __byte_order__ = '<'
+
+    class Antenna(RadiotapField):
         __hdr__ = (
             ('index', 'B', 0),
         )
 
-    class AntennaNoise(dpkt.Packet):
+    class AntennaNoise(RadiotapField):
         __hdr__ = (
             ('db', 'B', 0),
         )
 
-    class AntennaSignal(dpkt.Packet):
+    class AntennaSignal(RadiotapField):
         __hdr__ = (
             ('db', 'B', 0),
         )
 
-    class Channel(dpkt.Packet):
+    class Channel(RadiotapField):
+        __alignment__ = 2
         __hdr__ = (
             ('freq', 'H', 0),
             ('flags', 'H', 0),
         )
 
-    class FHSS(dpkt.Packet):
+    class FHSS(RadiotapField):
         __hdr__ = (
             ('set', 'B', 0),
             ('pattern', 'B', 0),
         )
 
-    class Flags(dpkt.Packet):
+    class Flags(RadiotapField):
         __hdr__ = (
             ('val', 'B', 0),
         )
@@ -311,84 +311,92 @@ class Radiotap(dpkt.Packet):
         def fcs(self, v): (v << _FCS_SHIFT) | (self.val & ~_FCS_MASK)
 
 
-    class LockQuality(dpkt.Packet):
+    class LockQuality(RadiotapField):
+        __alignment__ = 2
         __hdr__ = (
             ('val', 'H', 0),
         )
 
-    class RxFlags(dpkt.Packet):
+    class RxFlags(RadiotapField):
+        __alignment__ = 2
         __hdr__ = (
             ('val', 'H', 0),
         )
 
-    class Rate(dpkt.Packet):
+    class Rate(RadiotapField):
         __hdr__ = (
             ('val', 'B', 0),
         )
 
-    class TSFT(dpkt.Packet):
+    class TSFT(RadiotapField):
+        __alignment__ = 8
         __hdr__ = (
             ('usecs', 'Q', 0),
         )
 
-    class TxAttenuation(dpkt.Packet):
+    class TxAttenuation(RadiotapField):
+        __alignment__ = 2
         __hdr__ = (
             ('val', 'H', 0),
         )
 
-    class DbTxAttenuation(dpkt.Packet):
+    class DbTxAttenuation(RadiotapField):
+        __alignment__ = 2
         __hdr__ = (
             ('db', 'H', 0),
         )
 
-    class DbAntennaNoise(dpkt.Packet):
+    class DbAntennaNoise(RadiotapField):
         __hdr__ = (
             ('db', 'B', 0),
         )
 
-    class DbAntennaSignal(dpkt.Packet):
+    class DbAntennaSignal(RadiotapField):
         __hdr__ = (
             ('db', 'B', 0),
         )
 
-    class DbmTxPower(dpkt.Packet):
+    class DbmTxPower(RadiotapField):
         __hdr__ = (
             ('dbm', 'B', 0),
         )
 
+    class ChannelPlus(RadiotapField):
+        __alignment__ = 4
+        __hdr__ = (
+            ('flags', 'I', 0),
+            ('freq', 'H', 0),
+            ('channel', 'B', 0),
+            ('maxpower', 'B', 0),
+        )
+
 
 def test_Radiotap():
-    s = b'\x00\x00\x00\x18\x6e\x48\x00\x00\x00\x02\x6c\x09\xa0\x00\xa8\x81\x02\x00\x00\x00\x00\x00\x00\x00'
+    s = bytearray.fromhex('000030002f4000a0200800a0200800a020080000000000000884bdac2800000010028509a000a5000000a1009f01a102')
     rad = Radiotap(s)
     assert(rad.version == 0)
-    assert(rad.present_flags == 0x6e480000)
-    assert(rad.tsft_present == 0)
-    assert(rad.flags_present == 1)
-    assert(rad.rate_present == 1)
-    assert(rad.channel_present == 1)
-    assert(rad.fhss_present == 0)
-    assert(rad.ant_sig_present == 1)
-    assert(rad.ant_noise_present == 1)
-    assert(rad.lock_qual_present == 0)
-    assert(rad.db_tx_attn_present == 0)
-    assert(rad.dbm_tx_power_present == 0)
-    assert(rad.ant_present == 1)
-    assert(rad.db_ant_sig_present == 0)
-    assert(rad.db_ant_noise_present == 0)
-    assert(rad.rx_flags_present == 1)
-    assert(rad.channel.freq == 0x6c09)
-    assert(rad.channel.flags == 0xa000)
-    assert(len(rad.fields) == 7)
-
-
-def test_fcs():
-    s = b'\x00\x00\x1a\x00\x2f\x48\x00\x00\x34\x8f\x71\x09\x00\x00\x00\x00\x10\x0c\x85\x09\xc0\x00\xcc\x01\x00\x00'
-    rt = Radiotap(s)
-    assert(rt.flags_present == 1)
-    assert(rt.flags.fcs == 1)
+    assert(rad.present_flags == bytearray.fromhex('2f4000a0200800a0200800a020080000'))
+    assert(rad.tsft_present)
+    assert(rad.flags_present)
+    assert(rad.rate_present)
+    assert(rad.channel_present)
+    assert(not rad.fhss_present)
+    assert(rad.ant_sig_present)
+    assert(not rad.ant_noise_present)
+    assert(not rad.lock_qual_present)
+    assert(not rad.db_tx_attn_present)
+    assert(not rad.dbm_tx_power_present)
+    assert(not rad.ant_present)
+    assert(not rad.db_ant_sig_present)
+    assert(not rad.db_ant_noise_present)
+    assert(rad.rx_flags_present)
+    assert(rad.channel.freq == 2437)
+    assert(rad.channel.flags == 0x00a0)
+    assert(len(rad.fields) == 6)
+    assert(rad.flags_present)
+    assert(rad.flags.fcs)
 
 
 if __name__ == '__main__':
     test_Radiotap()
-    test_fcs()
     print('Tests Successful...')
