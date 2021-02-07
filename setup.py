3d4c9eb43a110aa1ac972d72ae14e08df82eced2
crocogorical <crocogorical@users.noreply.github.com>
2021-02-07 18:35:56 +0000

    Extend python versions testing run against. (#554)
    Include python 3.9, and also pypy3

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(name=package_name,
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
       ])
